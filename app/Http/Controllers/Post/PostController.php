b43a1f4db227849910e133145c752f58213667fd
kalpana udara <kalpanaudara058@gmail.com>
2024-11-02 14:09:15 +0530

    Create controller for post and defined in it route.

---
+++ app/Http/Controllers/Post/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function showPostCreateForm()
+    {
+        return 12 + 12;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Post\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'showPostCreateForm']);
