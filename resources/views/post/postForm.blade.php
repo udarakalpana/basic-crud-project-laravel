504c4c6495fd073e2b6869979d6391b26b40adde
kalpana udara <kalpanaudara058@gmail.com>
2024-11-02 14:48:57 +0530

    Removed tailwind class from blade.

--- resources/views/post/postForm.blade.php
+++ resources/views/post/postForm.blade.php
@@ -6,8 +6,24 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+        <form action="#">
+            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                <div class="sm:col-span-2">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
+                    <input type="text" name="name" id="name" class="input-field" placeholder="Post Title" required="">
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Content</label>
+                    <textarea id="description" rows="8" class="text-area" placeholder="Write post..."></textarea>
+                </div>
+            </div>
+            <button type="button" class="dark-btn">Create Post</button>
+        </form>
+    </div>
+</section>
 </body>
 </html>
