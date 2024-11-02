6a4ff71eea97a0e4fa7c117fdc8b00f7a6076f0c
kalpana udara <kalpanaudara058@gmail.com>
2024-11-02 14:19:30 +0530

    return post create balde file from post controller.

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function showPostCreateForm()
+    public function showPostCreateForm(): View
     {
-        return 12 + 12;
+        return view('post.postForm');
     }
 }
